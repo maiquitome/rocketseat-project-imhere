2dff112ab8bfc9e5c703046aab486b1cb9249fa3
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-10-14 06:59:11 -0300

    feat: :sparkles: add input to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
@@ -7,6 +7,14 @@ export function Home() {
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6b6b6b"
+        // keyboardType="ascii-capable" ONLY IOS
+        // keyboardType="visible-password" ONLY ANDROID
+      />
 
       <StatusBar style="auto" />
     </View>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -18,4 +18,12 @@ export const styles = StyleSheet.create({
     color: "#6b6b6b",
     fontSize: 16,
   },
+  input: {
+    height: 56,
+    backgroundColor: "#1f1e25",
+    borderRadius: 5,
+    color: "#fdfcfe",
+    padding: 16,
+    fontSize: 16,
+  },
 });
