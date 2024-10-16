960fe82c79f6757423ff997e27a5c1fd0a00b517
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-10-15 22:09:40 -0300

    feat: :sparkles: add TouchableOpacity

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,12 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, View, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
+  function handleParticipantAdd() {
+    console.log("Você clicou no botão de Adicionar");
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.eventName}>Nome do Evento</Text>
@@ -15,6 +19,10 @@ export function Home() {
         // keyboardType="ascii-capable" ONLY IOS
         // keyboardType="visible-password" ONLY ANDROID
       />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
 
       <StatusBar style="auto" />
     </View>
