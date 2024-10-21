fb3ec45c5163dafb4b5af62d84006bce710f101c
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-10-20 21:26:48 -0300

    feat: :lipstick: add form style

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -12,17 +12,19 @@ export function Home() {
       <Text style={styles.eventName}>Nome do Evento</Text>
       <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
 
-      <TextInput
-        style={styles.input}
-        placeholder="Nome do participante"
-        placeholderTextColor="#6b6b6b"
-        // keyboardType="ascii-capable" ONLY IOS
-        // keyboardType="visible-password" ONLY ANDROID
-      />
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6b6b6b"
+          // keyboardType="ascii-capable" ONLY IOS
+          // keyboardType="visible-password" ONLY ANDROID
+        />
 
-      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
-        <Text style={styles.buttonText}>+</Text>
-      </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
 
       <StatusBar style="auto" />
     </View>
