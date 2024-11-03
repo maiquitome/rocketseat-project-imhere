bdb9de8e743dfefa4912d3e65ced1ecb80db8c2c
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-03 14:57:34 -0300

    feat: :sparkles: add Alert

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,6 +5,7 @@ import {
   View,
   TouchableOpacity,
   FlatList,
+  Alert,
 } from "react-native";
 import { styles } from "./styles";
 
@@ -23,11 +24,25 @@ export function Home() {
   ];
 
   function handleParticipantAdd() {
-    console.log("Você clicou no botão de Adicionar");
+    if (participants.includes("Rodrigo")) {
+      return Alert.alert(
+        "Participante já existe",
+        "Já existe um participante na lista com esse nome"
+      );
+    }
   }
 
   function handleParticipantRemove(name: string) {
-    console.log(`Você clicou em remover o participante ${name}`);
+    Alert.alert("Remover participante", `Remover o participante ${name}?`, [
+      {
+        text: "Sim",
+        onPress: () => Alert.alert("Deletado"),
+      },
+      {
+        text: "Não",
+        style: "cancel",
+      },
+    ]);
   }
 
   return (
